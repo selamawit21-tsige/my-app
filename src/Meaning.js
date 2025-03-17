5669ad7b3218ae9749fef092e798262ebb4c694a
selamawit haileselasse <139425406+selamawit21-tsige@users.noreply.github.com>
2025-03-17 05:49:00 -0700

    adding css

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,22 +1,18 @@
 import React from "react";
-import Synonyms from "./synonyms";
-
+import "./Meaning.css";
+import Synonyms from "./synonyms"
 
 export default function Meaning(props) {
-  
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              <strong>Definition:</strong> {definition.definition}
-              <br />
-              <strong>Example:</strong>
-              <em>{definition.example}</em>
-              <Synonyms synonyms={definition.synonyms} />
-            </p>
+            <div className="definition">{definition.definition}</div>
+
+            <div className="example">{definition.example}</div>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
