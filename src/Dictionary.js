1a692bc0a3188a0360f298e26d1819d49cf30bb5
selamawit haileselasse <139425406+selamawit21-tsige@users.noreply.github.com>
2025-03-17 00:34:46 -0700

    add-axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,18 @@
+/* eslint-disable no-undef */
 import React, { useState } from "react";
 import "./Dictionary.css";
-
+import axios from "axios";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+      alert(`Searching for ${keyword} definition`);
+   //documentatio
+
+    // eslint-disable-next-line no-template-curly-in-string
+    let apiUrl = '4d9a2f9t09f9248b85e020e0o8b613c6 ${keyword}';
+       axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
